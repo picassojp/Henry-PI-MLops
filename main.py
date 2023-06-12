a233f3ed614419a95a80772bd9061144ebad1989
picassojp <picassojuanpablo@gmail.com>
2023-06-12 20:30:15 -0300

    detalles en main

--- main.py
+++ main.py
@@ -32,9 +32,6 @@ app = FastAPI(
         "email": "picassojuanpablo@gmail.com",
     },
 )
-# Se carga el df con los datos de las peliculas
-# df_movies = pd.read_csv(r'Datasets/movies_dataset_v2.csv', sep = ',', header = 0, parse_dates=["release_date"])
-#df_movies = pd.read_csv(r'Datasets/movies_dataset_v2.csv', sep = ',', header = 0, usecols=['id', 'title', 'release_date', 'cast', 'director', 'overview', 'popularity', 'vote_average', 'vote_count', 'return', 'release_year', 'budget', 'revenue'], parse_dates=["release_date"])
 
 df_movies_mesydia = pd.read_csv(r'Datasets/movies_dataset_v2_mesydia.csv', sep = ',', header = 0, parse_dates=["release_date"])
 df_movies_score = pd.read_csv(r'Datasets/movies_dataset_v2_score.csv', sep = ',', header = 0)
@@ -55,42 +52,38 @@ def cantidad_filmaciones_mes(mes:str):
     """
     Esta función devuelve la cantidad de películas que fueron estrenadas en el mes consultado (en idioma español)
     """
-    # función para convertir el mes en idioma español al formato adecuado para la consulta
-    def mes_to_numero(mes):
-        # Diccionario para mapear los nombres de los meses en español a su equivalente en inglés
-            meses_ingles = {
-                'enero': 'January',
-                'febrero': 'February',
-                'marzo': 'March',
-                'abril': 'April',
-                'mayo': 'May',
-                'junio': 'June',
-                'julio': 'July',
-                'agosto': 'August',
-                'septiembre': 'September',
-                'octubre': 'October',
-                'noviembre': 'November',
-                'diciembre': 'December'
-            }
-            
-            # se verifica si el mes se ingreso como str
-            #if isinstance(mes,str):
-            mes = mes.lower() #se pasa a minuscula
-            # else:
-            #     return print("Ingrese el nombre del mes que desea consultar. Ejemplo: 'julio'.")
-                    
-            #if mes in meses_ingles: #Verificar si el nombre del mes está en el diccionario
-            mes_ingles = meses_ingles[mes]
-                # Convertir el nombre del mes a formato de fecha
-            numero_mes = int(datetime.strptime(mes_ingles, "%B").strftime("%m"))
-            return numero_mes            
-            #     return print("Ingrese el nombre del mes que desea consultar. Ejemplo: 'julio'.")
+    # Diccionario para mapear los nombres de los meses en español a su equivalente en inglés
+    meses_ingles = {
+        'enero': 'January',
+        'febrero': 'February',
+        'marzo': 'March',
+        'abril': 'April',
+        'mayo': 'May',
+        'junio': 'June',
+        'julio': 'July',
+        'agosto': 'August',
+        'septiembre': 'September',
+        'octubre': 'October',
+        'noviembre': 'November',
+        'diciembre': 'December'
+    }
     
-    n_mes = mes_to_numero(mes)
+    # se verifica si el mes se ingreso como str
+    if isinstance(mes,str):
+        mes = mes.lower() #se pasa a minuscula
+    else:
+        return "Ingrese el nombre del mes que desea consultar. Ejemplo: 'julio'."
+            
+    if mes in meses_ingles: #Verificar si el nombre del mes está en el diccionario
+        mes_ingles = meses_ingles[mes]
+        # Convertir el nombre del mes a formato de fecha
+        n_mes = int(datetime.strptime(mes_ingles, "%B").strftime("%m"))           
+    else:
+        return "Ingrese el nombre del mes que desea consultar. Ejemplo: 'julio'."
+    
     cant = int(df_movies_mesydia[df_movies_mesydia.release_date.dt.month == n_mes].id.count())
       
     return {'mes': mes, 'cantidad': cant}  
-    # return f'{str(df_movies[df_movies.release_date.dt.month == mes_to_numero(mes)].id.count())} películas se estrenaron en el mes de {mes}'
 
 # cantidad_filmaciones_dia
 @app.get("/cantidad_filmaciones_dia/{dia}")
@@ -98,31 +91,27 @@ def cantidad_filmaciones_dia(dia:str):
     """
     Esta función devuelve la cantidad de películas que fueron estrenadas en el día consultado (en idioma español)
     """
-    # función para convertir el mes en idioma español al formato adecuado para la consulta    
-    def eng_day(dia):
     # Diccionario para mapear los nombres de los días en español con el número (inicio semana lunes = 0)
-        dias_ingles = {
-            'lunes': 0,
-            'martes': 1,
-            'miércoles': 2,
-            'jueves': 3,
-            'viernes': 4,
-            'sábado': 5,
-            'domingo': 6
-        }
-        # se verifica si el dia se ingreso como str
-        if isinstance(dia,str):
-            dia = dia.lower() #se pasa a minuscula
-        # else:
-        #     return print("Ingrese el nombre del día que desea consultar. Ejemplo: 'martes'.")
-        if dia in dias_ingles: #Verificar si el día es correcto
-            dia_ingles = dias_ingles[dia]          
-            return int(dia_ingles)
-        # else:
-        #     return print("Ingrese el nombre del día que desea consultar. Ejemplo: 'martes'.")
-    n_dia = eng_day(dia)
+    dias_ingles = {
+        'lunes': 0,
+        'martes': 1,
+        'miércoles': 2,
+        'jueves': 3,
+        'viernes': 4,
+        'sábado': 5,
+        'domingo': 6}
+    
+    # se verifica si el dia se ingreso como str
+    if isinstance(dia,str):
+        dia = dia.lower() #se pasa a minuscula
+    else:
+        return "Ingrese el nombre del día que desea consultar. Ejemplo: 'martes'."
+    if dia in dias_ingles: #Verificar si el día es correcto
+        dia_ingles = dias_ingles[dia]          
+        n_dia=int(dia_ingles)
+    else:
+        return "Ingrese el nombre del día que desea consultar. Ejemplo: 'martes'."
     cant = int(df_movies_mesydia[df_movies_mesydia.release_date.dt.weekday == n_dia].shape[0])
-    # return f'{str(df_movies[df_movies.release_date.dt.weekday == eng_day(dia)].shape[0])} películas se estrenaron en un día {dia}'
     return {'dia': dia, 'cantidad': cant}
 
 @app.get("/score_titulo/{titulo}")
@@ -132,18 +121,17 @@ def score_titulo(titulo:str):
     """
     # se verifica si el titulo es un str
     if isinstance(titulo,str):
-        titulo = titulo.lower() #se pasa a minuscula
+        titulo = titulo.lower() #se pasa a minúscula
     else:
         return f'No fue posible encontrar el título {str(titulo)} en nuestra base de datos. Verificar si es correcto o probar con un título alternativo en español.'
     df_score = df_movies_score.loc[df_movies_score.title.str.lower().str.contains(titulo),["title", "release_year", "popularity"]].iloc[0]
-    print(df_score)
+    
     titulo = str(df_score.title)
     anio = int(df_score.release_year)
     score = float(df_score.popularity.round(2))
     if anio is None:
         return f'No fue posible encontrar el título {titulo} en nuestra base de datos. Verificar si es correcto o probar con un título alternativo en español.'
     else:
-        # return f'La película "{titulo}" fue estrenada en el año {str(year)} y tiene una popularidad de {str(score)}'
         return {'titulo': titulo, 'anio': anio, 'popularidad': score}
 
 @app.get("/votos_titulo/{titulo}")
@@ -168,7 +156,6 @@ def votos_titulo(titulo:str):
     elif voto_total < 2000:
         return f'El título {titulo} no cuenta con la cantidad suficiente de valoraciones por lo que no es posible informar la valoración promedio.'
     else:
-        #return f'La película "{titulo}" fue estrenada en el año {year}. La valoración promedio es de {voto_promedio}, resultado de {votos_cant} votaciones.'
         return {'titulo': titulo, 'anio': anio, 'voto_total': voto_total, 'voto_promedio': voto_promedio}
 
 
@@ -182,11 +169,10 @@ def get_actor(nombre_actor:str):
     else:
         return f'No fue posible encontrar el actor "{nombre_actor}" en nuestra base de datos. Verificar si es correcto o probar con un nombre alternativo en español.'
     
-    fila = ast.literal_eval(df_movies_actor.loc[df_movies_actor.actor.str.lower().str.contains(nombre_actor),"actor"].iloc[0])
+    fila = ast.literal_eval(df_movies_actor.loc[df_movies_actor.actor.str.lower().str.contains(nombre_actor),"actor"].iloc[0]) #el str de actores se pasa a una lista para poder recorrerse posteriormente
     for i in fila:
         if nombre_actor in i.lower():
-            nombre_actor = i
-            print(nombre_actor)
+            nombre_actor = i #se guarda el nombre del actor tal como figura en la columna
     cant = int(df_movies_actor.loc[df_movies_actor.actor.str.contains(nombre_actor),"id"].shape[0])
     retorno_total = float(df_movies_actor.loc[df_movies_actor.actor.str.contains(nombre_actor),"return"].sum().round(2))
     retorno_prom = float(df_movies_actor.loc[df_movies_actor.actor.str.contains(nombre_actor),"return"].mean().round(2))
@@ -224,7 +210,6 @@ def get_director(nombre_director:str):
     if cant is None:
         return f'No fue posible encontrar películas del director "{nombre_director}" en nuestra base de datos. Verificar si es correcto o probar con un nombre alternativo en español.'
     else:
-        # return f'El director "{nombre_director}" ha alcanzado un retorno total de {retorno_total} con sus películas, lo que da una idea de su éxito. Las películas que dirigió fueron: \n {peliculas}'
         return {'director': nombre_director, 'retorno_total_director': retorno_total, 'peliculas': peliculas, 'anio': anios, 'retorno_pelicula': retornos,'budget_pelicula':budget,'revenue_pelicula':revenue}
 
 
